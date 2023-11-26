d2f45bac991c240f843b70962ac2dfb22a35ef06
Alex Alext <alextoma8032002@yahoo.com>
2023-11-26 20:08:20 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Toma_Alexandru_Lab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Toma_Alexandru_Lab7;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
